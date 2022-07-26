2e957765b9dc95bb0b52055c978bb7db09082050
cbkpar <cbkpar@naver.com>
2022-07-27 03:57:21 +0900

    on_message test

--- bot.py
+++ bot.py
@@ -9,8 +9,13 @@ client = commands.Bot(command_prefix = '/')
 async def on_ready():
 
   await client.change_presence(status=discord.Status.online)
-  await client.change_presence(activity=discord.Game(name="게임 하는중"))
+  await client.change_presence(activity=discord.Game(name="비트아지트"))
   print("봇 이름:",client.user.name,"봇 아이디:",client.user.id,"봇 버전:",discord.__version__)
+
+@client.event
+async def on_message(message):
+    if message.content.startswith("ping"):
+        await message.channel.send("pong")
 
 @client.command(name='주사위')
 async def roll(ctx, number: int):
@@ -19,5 +24,6 @@ async def roll(ctx, number: int):
 @roll.error
 async def roll_error(ctx, error):
     await ctx.send(f"2 이상의 정수를 넣어주세요!\nex) /주사위 6")
+
 
 client.run(os.environ['token'])
