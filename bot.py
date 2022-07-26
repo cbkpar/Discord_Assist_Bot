8930be54b5135621c5e06b9bf4269710c7e8d79e
cbkpar <cbkpar@naver.com>
2022-07-27 03:24:25 +0900

    Dice Bug Update

--- bot.py
+++ bot.py
@@ -1,5 +1,6 @@
 import discord
 from discord.ext import commands
+import random
 import os
 
 client = commands.Bot(command_prefix = '/')
@@ -7,18 +8,16 @@ client = commands.Bot(command_prefix = '/')
 @client.event
 async def on_ready():
 
-  # [discord.Status.online = 온라인],[discord.Status.idle = 자리비움],[discord.Status.dnd = 다른용무],[discord.Status.offline = 오프라인]
   await client.change_presence(status=discord.Status.online)
-
   await client.change_presence(activity=discord.Game(name="게임 하는중"))
-  #await client.change_presence(activity=discord.Streaming(name="스트림 방송중", url='링크'))
-  #await client.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name="노래 듣는중"))
-  #await client.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="영상 시청중"))
-  
   print("봇 이름:",client.user.name,"봇 아이디:",client.user.id,"봇 버전:",discord.__version__)
 
 @client.command(name='주사위')
-async def roll(ctx, number):
-    await ctx.send(f'주사위를 굴려 {random.randint(1,int(number))}이(가) 나왔습니다. (1-{number})')
+async def roll(ctx, number: int):
+    await ctx.send(f'주사위를 굴려 {random.randint(1,number)}이(가) 나왔습니다. (1-{number})')
+
+@roll.error
+async def roll_error(ctx, error):
+    await ctx.send(f"2 이상의 정수를 넣어주세요!\nex) /주사위 6")
 
 client.run(os.environ['token'])
