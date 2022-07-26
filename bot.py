a4cd9b39a6213699271cb97d85b5ab3ba1804b76
cbkpar <cbkpar@naver.com>
2022-07-27 04:47:08 +0900

    Bug Fixed

--- bot.py
+++ bot.py
@@ -17,6 +17,9 @@ async def on_message(message):
     if message.content.startswith("!ping"):
         await message.channel.send("pong3")
 
+    if message.content.startswith("!시놀로지"):
+        await message.channel.send("시놀로지 : <http://jusin.synology.me:5000/>")
+
     if message.content.startswith("!문제뽑기"):
         info = message.content[6:]
         infosplit = info.split("/")
@@ -26,12 +29,14 @@ async def on_message(message):
         problem = problems.split(" ")
         random.shuffle(problem)
         output = ""
+        iCount = 1
         for i in range(0, len(person)):
-          output += person[i] + "------------>" + problem[i] + "번 (<https://www.acmicpc.net/problem/"+problem[i]+">)\n"
+          output += iCount+". "person[i] + " : " + problem[i] + "번 (<https://www.acmicpc.net/problem/"+problem[i]+">)\n"
+          iCount += 1
         await message.channel.send(f'{output}')
         
     if message.content.startswith("!주사위"):
-        msgsplit = message.split(" ")
+        msgsplit = message.content.split(" ")
         number = int(msgsplit[1])
         if number >= 2:
           await message.channel.send(f'주사위를 굴려 {random.randint(1,number)}이(가) 나왔습니다. (1-{number})')
