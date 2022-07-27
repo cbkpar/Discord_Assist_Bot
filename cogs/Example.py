065a705366d42d1de996c1c1e03d1a29559b9fec
cbkpar <cbkpar@naver.com>
2022-07-28 05:25:16 +0900

    Add Example

---
+++ cogs/Example.py
@@ -0,0 +1,12 @@
+from discord.ext import commands
+
+class Example(commands.Cog):
+    def __init__(self, client):
+        self.client = client
+
+    @commands.Cog.listener()
+    async def on_ready(self):
+        print("example Cog is Ready")
+
+def setup(client):
+    client.add_cog(Example(client))
