ffc04ebe86afc315ce88ab10d9e6991b02e7a6e7
cbkpar <cbkpar@naver.com>
2022-07-28 06:00:26 +0900

    ping pong test

--- cogs/Example.py
+++ cogs/Example.py
@@ -8,10 +8,10 @@ class Example(commands.Cog):
     async def on_ready(self):
         print("example Cog is Ready")
         
-    @commands.Cog.event
-    async def on_message(message):
-      await commands.Cog.process_commands(message)
-      await message.channel.send("pong1")
+    @commands.command(name="ping")
+    async def restaurant(self, ctx):
+      embed = discord.Embed(title=nowDate + ' pong.', description=descript, color=discord.Color.blue())
+      await ctx.send(embed=embed)
 
 
 def setup(client):
