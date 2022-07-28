aefa3b319a071cd1f60710e7888ceb8f4b0bf23e
cbkpar <cbkpar@naver.com>
2022-07-29 03:30:49 +0900

    Bug Fixed

--- cogs/Music.py
+++ cogs/Music.py
@@ -41,7 +41,7 @@ class Music(commands.Cog):
             'options': '-vn',
             "before_options": "-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5"
         }
-        player = discord.FFmpegPCMAudio(link, **ffmpeg_options, executable = ffmpeg)
+        player = discord.FFmpegPCMAudio(link, **ffmpeg_options)
         ctx.voice_client.play(player)
         
         embed = discord.Embed(title = '음악 재생', description = f'{title} 재생을 시작힐게요!' , color = discord.Color.blue())
