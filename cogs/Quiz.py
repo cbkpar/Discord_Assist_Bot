1052c335564b0066dbdb25cea46f12e14fd07cfe
cbkpar <cbkpar@naver.com>
2022-07-29 01:06:47 +0900

    Adding Nonsense

--- cogs/Quiz.py
+++ cogs/Quiz.py
@@ -9,6 +9,9 @@ class Quiz(commands.Cog):
         self.client = client
         with open("./data/quiz.json", 'r', encoding='utf-8') as f:
             self.quizDict = json.load(f)
+            
+        with open("./data/nonsense.json", 'r', encoding='utf-8') as f:
+            self.quiznonsense = json.load(f)
 
     @commands.Cog.listener()
     async def on_ready(self):
@@ -19,6 +22,24 @@ class Quiz(commands.Cog):
         problemList = list(self.quizDict.keys())
         problem = random.choice(problemList)
         answer = self.quizDict[problem]
+        await ctx.send(problem)
+
+        def checkAnswer(message):
+            if message.channel == ctx.channel and answer == message.content:
+                return True
+            else:
+                return False
+        try:
+            await self.client.wait_for("message", timeout = 10.0, check = checkAnswer)
+            await ctx.send("정답이에요!")
+        except asyncio.TimeoutError:
+            await ctx.send(f'땡! 시간초과에요! [정답 : {answer}]')
+
+    @commands.command(name ="넌센스")
+    async def quiz_nonsense(self, ctx):
+        problemList = list(self.quiznonsense.keys())
+        problem = random.choice(problemList)
+        answer = self.quiznonsense[problem]
         await ctx.send(problem)
 
         def checkAnswer(message):
