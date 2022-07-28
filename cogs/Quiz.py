1f89b541b2ff18eb86a23299c060ba4b74163e3a
cbkpar <cbkpar@naver.com>
2022-07-28 23:34:50 +0900

    Quiz Update

--- cogs/Quiz.py
+++ cogs/Quiz.py
@@ -7,7 +7,12 @@ import random
 class Quiz(commands.Cog):
     def __init__(self, client):
         self.client = client
-    
+    		self.quizDict = {}
+		    with open("./data/quiz.csv", 'r', encoding='utf-8') as f:
+		        reader = csv.reader(f)
+		        for row in reader:
+		            self.quizDict[row[0]] = row[1]
+                
     @commands.Cog.listener()
     async def on_ready(self):
         print("Quiz Cog is Ready")
