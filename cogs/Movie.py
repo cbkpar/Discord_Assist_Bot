dbd8e0e7838af8f34eda718fc26f3212e1d2b6ad
cbkpar <cbkpar@naver.com>
2022-07-30 01:55:02 +0900

    Movie update

--- cogs/Movie.py
+++ cogs/Movie.py
@@ -14,6 +14,8 @@ class Movie(commands.Cog):
 
     @commands.command(name ="영화")
     async def movie(self, ctx):
+      
+      await ctx.send('movie test!')
       try:
         response = requests.get("http://www.cgv.co.kr/movies/")
 		    response.encoding = 'utf-8'
@@ -23,7 +25,7 @@ class Movie(commands.Cog):
         infos = soup.select('tr')
         
         for info in infos[1:10]:
-            분류, 번호, 제목, 작성자, 작성일, 조회 = 정보.select('td')
+            분류, 번호, 제목, 작성자, 작성일, 조회 = info.select('td')
             await ctx.send(f'```\n제목 : {제목.text}\n날짜 : {작성일.text}```\n링크 : http://www.cgv.co.kr/movies/{제목.a["href"]}\n--------------------------------------')
       except:
         await ctx.send(f'{today.month}월 {today.day}일 {today.hour}시 {today.minute}분 CGV 오류 발생')
