d6352f24afbf8aeded3cb98f933d43c6176da36a
cbkpar <cbkpar@naver.com>
2022-07-30 02:13:04 +0900

    requirements update

--- cogs/Movie.py
+++ cogs/Movie.py
@@ -15,7 +15,17 @@ class Movie(commands.Cog):
     @commands.command(name ="영화")
     async def _movie(self, ctx):
         await ctx.send('movie test!')
-
+        try:
+            response = requests.get("http://www.cgv.co.kr/movies/")
+            response.encoding = 'utf-8'
+            html = response.text
+            soup = BeautifulSoup(html, 'html.parser')
+            infos = soup.select('tr')
+          for info in infos[1:10]:
+              분류, 번호, 제목, 작성자, 작성일, 조회 = info.select('td')
+              await ctx.send(f'```\n제목 : {제목.text}\n날짜 : {작성일.text}```\n링크 : http://www.cgv.co.kr/movies/{제목.a["href"]}\n--------------------------------------')
+        except:
+            await ctx.send(f'{today.month}월 {today.day}일 {today.hour}시 {today.minute}분 CGV 오류 발생')
 
 def setup(client):
     client.add_cog(Movie(client))
